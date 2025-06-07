4bd40c98fbfa0e899e30b676c802c058eee9511f
Moshe Schwartzberg <43815731+lionsgrandson@users.noreply.github.com>
2025-06-07 22:54:18 +0300

    edit: removed spacing from cards

--- src/componens/pages/Card.jsx
+++ src/componens/pages/Card.jsx
@@ -1,37 +1,50 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
+import React from "react";
+import { useTranslation } from "react-i18next";
 import {
   MDBCard,
   MDBCardBody,
   MDBCardTitle,
   MDBCardText,
-  MDBCardImage
-} from 'mdb-react-ui-kit';
+  MDBCardImage,
+} from "mdb-react-ui-kit";
 
-import '../style.css'
+import "../style.css";
 
-export default function Card({img_url,kind}) {
-    const {t}=useTranslation()
+export default function Card({ img_url, kind }) {
+  const { t } = useTranslation();
 
-    const text_style={
-        fontStyle: "italic",
-        fontSize:"large",
-        textShadow: "1px 1px 1px rgb(180, 180, 180)",
-        fontFamily: "system-ui sans-serif",
-      }
+  const text_style = {
+    fontStyle: "italic",
+    fontSize: "large",
+    textShadow: "1px 1px 1px rgb(180, 180, 180)",
+    fontFamily: "system-ui sans-serif",
+  };
 
   return (
-    <div  style={{width:"350px",height:"400px",margin:'10vmin',marginBottom:"20vmax"}}>
-        <MDBCard>
-      <MDBCardImage src= {img_url} position='top'  alt={kind} />
-      <MDBCardBody>
-        <MDBCardTitle style={{textTransform:"uppercase",textDecoration:"underline",fontWeight:"600"}}>{t(kind)}</MDBCardTitle>
-        <MDBCardText style={text_style}>
-          {t(`${kind} home text`)}
-        </MDBCardText>
-      </MDBCardBody>
-    </MDBCard>
+    <div
+      style={{
+        width: "min-content",
+        minWidth: "20rem",
+        height: "400px",
+        margin: "10vmax auto ",
+        marginBottom: "10vmax",
+      }}
+    >
+      <MDBCard>
+        <MDBCardImage src={img_url} position="top" alt={kind} />
+        <MDBCardBody>
+          <MDBCardTitle
+            style={{
+              textTransform: "uppercase",
+              textDecoration: "underline",
+              fontWeight: "600",
+            }}
+          >
+            {t(kind)}
+          </MDBCardTitle>
+          <MDBCardText style={text_style}>{t(`${kind} home text`)}</MDBCardText>
+        </MDBCardBody>
+      </MDBCard>
     </div>
   );
 }
-
