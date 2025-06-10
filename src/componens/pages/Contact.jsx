6c34267d0aecae890f93a28e5dc168f9101f33e6
Moshe Schwartzberg <43815731+lionsgrandson@users.noreply.github.com>
2025-06-10 21:06:17 +0300

    edit: contact form text is white to match the background picture

--- src/componens/pages/Contact.jsx
+++ src/componens/pages/Contact.jsx
@@ -1,35 +1,29 @@
-import React from 'react';
-import { MDBInput, MDBCheckbox, MDBBtn,MDBTextArea } from 'mdb-react-ui-kit';
-import { useTranslation } from 'react-i18next';
+import React from "react";
+import { MDBInput, MDBCheckbox, MDBBtn, MDBTextArea } from "mdb-react-ui-kit";
+import { useTranslation } from "react-i18next";
 import { useRef, useEffect, useState } from "react";
 import emailjs from "@emailjs/browser";
-
-
+import { firstImage } from "../../assets";
 
 export default function App() {
-
-
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [subject, setSubject] = useState("");
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
 
+  const { t } = useTranslation();
 
-
-  const {t}=useTranslation()
-
-  const style={
-    height:"80vmin",
-    minHeight:"500px",
-    backgroundImage: `url("https://img.freepik.com/free-vector/stylish-white-background-with-diagonal-lines_1017-33199.jpg?w=1060&t=st=1707657485~exp=1707658085~hmac=21438aa14083611e8593ff1623f1db27998eb8d881efe4d774ba7f8b52a50be4")`,
-    backgroundRepeat:"no-repeat",
-    backgroundSize:"cover"
-  }
-
+  const style = {
+    height: "80vmin",
+    minHeight: "500px",
+    backgroundImage: `url(${firstImage})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    color: "white",
+  };
 
   useEffect(() => emailjs.init("WRe1AxRZRD1p_fgpj"), []);
-
 
   const handleClick = async (e) => {
     e.preventDefault();
@@ -38,41 +32,68 @@ export default function App() {
     try {
       setLoading(true);
       await emailjs.send(serviceId, templateId, {
-       name: name,
-       message: message,
-       subject: subject,
-       email:email
+        name: name,
+        message: message,
+        subject: subject,
+        email: email,
       });
       alert("email successfully sent check inbox");
     } catch (error) {
       console.log(error);
     } finally {
       setLoading(false);
-
-
     }
   };
   return (
+    <div
+      className="col-12 d-flex flrx-column justify-content-center align-items-center"
+      style={style}
+    >
+      <form
+        id="form"
+        className="text-center"
+        style={{
+          width: "100%",
+          maxWidth: "300px",
+          fontStyle: "italic",
+          color: "white !important",
+        }}
+      >
+        <h1>{t("contact us")}</h1>
 
-    <div className='col-12 d-flex flrx-column justify-content-center align-items-center' style={style}>
+        <MDBInput
+          label={t("name")}
+          v-model="name"
+          wrapperClass="mb-4"
+          onChange={(e) => setName(e.target.value)}
+        />
 
-    <form id='form' className='text-center' style={{ width: '100%', maxWidth: '300px',fontStyle:'italic' }}>
-      <h1>{t('contact us')}</h1>
+        <MDBInput
+          type="email"
+          style={{ color: "white" }}
+          label={t("email")}
+          v-model="email"
+          wrapperClass="mb-4"
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-      <MDBInput label={t('name')} v-model='name' wrapperClass='mb-4' onChange={(e) => setName(e.target.value)} />
+        <MDBInput
+          label={t("subject")}
+          v-model="subject"
+          wrapperClass="mb-4"
+          onChange={(e) => setSubject(e.target.value)}
+        />
 
-      <MDBInput type='email' label={t('email')} v-model='email' wrapperClass='mb-4' onChange={(e) => setEmail(e.target.value)} />
+        <MDBTextArea
+          wrapperClass="mb-4"
+          label={t("message")}
+          onChange={(e) => setMessage(e.target.value)}
+        />
 
-      <MDBInput label={t('subject')} v-model='subject' wrapperClass='mb-4'onChange={(e) => setSubject(e.target.value)} />
-
-      <MDBTextArea wrapperClass='mb-4' label={t('message')} onChange={(e) => setMessage(e.target.value)} />
-
-
-      <MDBBtn color='primary' block className='my-4 ' onClick={handleClick}>
-        {t('send')}
-      
-      </MDBBtn>
-    </form>
+        <MDBBtn color="primary" block className="my-4 " onClick={handleClick}>
+          {t("send")}
+        </MDBBtn>
+      </form>
     </div>
   );
 }
