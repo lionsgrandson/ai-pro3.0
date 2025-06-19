44c483d2437eedce6a40c7b80ed633a1737966ef
Moshe Schwartzberg <43815731+lionsgrandson@users.noreply.github.com>
2025-06-19 10:14:34 +0300

    edit: spacing issue with the header video

--- src/componens/pages/Lcp.jsx
+++ src/componens/pages/Lcp.jsx
@@ -1,45 +1,38 @@
-import React from 'react'
+import React from "react";
 import Videobg from "../../assets/video_home_com.mp4";
 import { useTranslation } from "react-i18next";
 
 export default function Lcp() {
-    const {i18n,t} =useTranslation()
+  const { i18n, t } = useTranslation();
 
-    const dir = i18n.dir();
+  const dir = i18n.dir();
 
-    const slogan_style = {
-      fontFamily: "system-ui sans-serif",
-      fontWeight: "bold",
-      fontStyle: "italic",
-      textShadow: "3px 1px 2px black",
-      wordSpacing: "100vmax",
-      fontSize: "10vmin",
-      direction: { dir },
-    };
-
+  const slogan_style = {
+    fontFamily: "system-ui sans-serif",
+    fontWeight: "bold",
+    fontStyle: "italic",
+    textShadow: "3px 1px 2px black",
+    wordSpacing: "100vmax",
+    fontSize: "10vmin",
+    direction: { dir },
+  };
 
   return (
     <div className=" d-flex flex-column align-items-center col-12">
-    <video
-      src={Videobg}
-      autoPlay
-      loop
-      muted
-      style={{ objectFit: "cover", width: "100%", height: "100vh" }}
-    />
-    <div
-      className="col-12 position-absolute flex-column justify-content-around d-flex align-items-center"
-      style={{
-        padding: "10vh",
-        objectFit: "cover",
-        width: "100%",
-        height: "100vh",
-      }}
-    >
-      <div className="text-light" style={{ fontSize: "30px" }}>
-        <h1 style={slogan_style}>{t("slogen")}</h1>
+      <video src={Videobg} autoPlay loop muted className="videoHeader" />
+      <div
+        className="col-12 position-absolute flex-column justify-content-around d-flex align-items-center"
+        style={{
+          padding: "10vh",
+          objectFit: "cover",
+          width: "100%",
+          height: "100vh",
+        }}
+      >
+        <div className="text-light" style={{ fontSize: "30px" }}>
+          <h1 style={slogan_style}>{t("slogen")}</h1>
+        </div>
       </div>
     </div>
-  </div>
-  )
+  );
 }
