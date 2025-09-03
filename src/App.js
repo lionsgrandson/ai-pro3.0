399632fc0d1c93b167e16d5fa5b3ca49d1953bb9
Moshe Schwartzberg <43815731+lionsgrandson@users.noreply.github.com>
2025-09-03 17:18:45 +0300

    edit: revert to original path

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import "react-bootstrap/dist/react-bootstrap.min.js";
-import { Route, Routes } from "react-router-dom";
-import Home from "./componens/pages/Home";
-import Header from "./componens/Header";
-import Calibration from "./componens/pages/Calibration";
-import Validation from "./componens/pages/Validation";
-import Repair from "./componens/pages/Repair";
-import Engineering from "./componens/pages/Engineering";
-import Contact from "./componens/pages/Contact";
-import Footer from "./componens/Footer";
-import { FloatingWhatsApp } from "react-floating-whatsapp";
+import 'react-bootstrap/dist/react-bootstrap.min.js'
+import { Route, Routes } from 'react-router-dom'
+import Home from './componens/pages/Home'
+import Header from './componens/Header'
+import Calibration from './componens/pages/Calibration'
+import Validation from './componens/pages/Validation'
+import Repair from './componens/pages/Repair'
+import Engineering from './componens/pages/Engineering'
+import Contact from './componens/pages/Contact'
+import Footer from './componens/Footer'
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
 
 function App() {
   return (
     <div>
       <FloatingWhatsApp
-        phoneNumber="972505100018"
-        accountName="AI Pro"
-        avatar="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/San_Diego_Comic-Con_2024_Masquerade_-_Cosplay_of_Batman_3.jpg/960px-San_Diego_Comic-Con_2024_Masquerade_-_Cosplay_of_Batman_3.jpg"
+        phoneNumber='972505100018'
+        accountName='AI Pro'
+        avatar='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/San_Diego_Comic-Con_2024_Masquerade_-_Cosplay_of_Batman_3.jpg/960px-San_Diego_Comic-Con_2024_Masquerade_-_Cosplay_of_Batman_3.jpg'
       />
       <Header />
       <Routes>
-        <Route exact path="/ai-pro-temp/" element={<Home />} />
-        <Route path="/ai-pro-temp/calibration" element={<Calibration />} />
-        <Route path="/ai-pro-temp/engineering" element={<Engineering />} />
-        <Route path="/ai-pro-temp/repair" element={<Repair />} />
-        <Route path="/ai-pro-temp/validation" element={<Validation />} />
-        <Route path="/ai-pro-temp/contact" element={<Contact />} />
+        <Route exact path='/' element={<Home />} />
+        <Route path='/calibration' element={<Calibration />} />
+        <Route path='/engineering' element={<Engineering />} />
+        <Route path='/repair' element={<Repair />} />
+        <Route path='/validation' element={<Validation />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
       <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/componens/Header.jsx
+++ src/componens/Header.jsx
@@ -1,123 +1,123 @@
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { logo_cut, israel_color, usa_color } from "../assets";
-import { useTranslation } from "react-i18next";
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { logo_cut, israel_color, usa_color } from '../assets'
+import { useTranslation } from 'react-i18next'
 import {
   MDBFooter,
   MDBContainer,
   MDBRow,
   MDBCol,
   MDBIcon,
-} from "mdb-react-ui-kit";
+} from 'mdb-react-ui-kit'
 export default function Header() {
-  const [expanded, setExpanded] = useState(false);
-  const { t, i18n } = useTranslation();
+  const [expanded, setExpanded] = useState(false)
+  const { t, i18n } = useTranslation()
 
-  const dir = i18n.dir();
+  const dir = i18n.dir()
 
   const changeLanguage = (lng) => {
-    i18n.changeLanguage(lng);
-  };
+    i18n.changeLanguage(lng)
+  }
 
   return (
     <div
       style={{
-        direction: "rtl",
-        position: "fixed",
-        width: " 100vw",
-        zIndex: "1",
+        direction: 'rtl',
+        position: 'fixed',
+        width: ' 100vw',
+        zIndex: '1',
       }}
     >
       <Navbar
         collapseOnSelect
-        expand="md"
-        bg="light"
-        variant="light"
+        expand='md'
+        bg='light'
+        variant='light'
         expanded={expanded}
-        id="togNav"
-        style={{ direction: dir, flexWrap: "wrap" }}
+        id='togNav'
+        style={{ direction: dir, flexWrap: 'wrap' }}
       >
         <Navbar.Brand>
-          <div style={{ marginLeft: "20px" }}>
-            <a href="/ai-pro-temp">
+          <div style={{ marginLeft: '20px' }}>
+            <a href='/ai-pro-temp'>
               {/* TODO when changing the links back remove the ai-pro0temp */}
               <img
                 src={logo_cut}
-                style={{ width: "40px", borderRadius: "50%" }}
-                alt="brand logo"
+                style={{ width: '40px', borderRadius: '50%' }}
+                alt='brand logo'
               />
             </a>
           </div>
         </Navbar.Brand>
         <Navbar.Toggle
-          aria-controls="responsive-navbar-nav"
-          style={{ marginRight: "20px" }}
-          onClick={() => setExpanded(expanded ? false : "expanded")}
+          aria-controls='responsive-navbar-nav'
+          style={{ marginRight: '20px' }}
+          onClick={() => setExpanded(expanded ? false : 'expanded')}
         />
-        <Navbar.Collapse id="responsive-navbar-nav" style={{ direction: dir }}>
-          <Nav className="me-auto my-2 my-lg-0 text-secondary d-flex align-items-center col-10">
-            <Nav.Link as={Link} to="/ai-pro-temp/" className="test2">
-              <strong>{t("home").toUpperCase()}</strong>
+        <Navbar.Collapse id='responsive-navbar-nav' style={{ direction: dir }}>
+          <Nav className='me-auto my-2 my-lg-0 text-secondary d-flex align-items-center col-10'>
+            <Nav.Link as={Link} to='/' className='test2'>
+              <strong>{t('home').toUpperCase()}</strong>
             </Nav.Link>
-            <Nav.Link as={Link} to="/ai-pro-temp/calibration" className="test2">
-              <strong>{t("calibration").toUpperCase()}</strong>
+            <Nav.Link as={Link} to='/calibration' className='test2'>
+              <strong>{t('calibration').toUpperCase()}</strong>
             </Nav.Link>
-            <Nav.Link as={Link} to="/ai-pro-temp/validation" className="test2">
-              <strong>{t("validation").toUpperCase()}</strong>
+            <Nav.Link as={Link} to='/validation' className='test2'>
+              <strong>{t('validation').toUpperCase()}</strong>
             </Nav.Link>
-            <Nav.Link as={Link} to="/ai-pro-temp/engineering" className="test2">
-              <strong>{t("engineering").toUpperCase()}</strong>
+            <Nav.Link as={Link} to='/engineering' className='test2'>
+              <strong>{t('engineering').toUpperCase()}</strong>
             </Nav.Link>
-            <Nav.Link as={Link} to="/ai-pro-temp/repair" className="test2">
-              <strong>{t("repair services").toUpperCase()}</strong>
+            <Nav.Link as={Link} to='/repair' className='test2'>
+              <strong>{t('repair services').toUpperCase()}</strong>
             </Nav.Link>
-            <Nav.Link as={Link} to="/ai-pro-temp/contact" className="test2">
-              <strong>{t("contact").toUpperCase()}</strong>
+            <Nav.Link as={Link} to='/contact' className='test2'>
+              <strong>{t('contact').toUpperCase()}</strong>
             </Nav.Link>
-            <div style={{ display: "flex" }} className="test2">
+            <div style={{ display: 'flex' }} className='test2'>
               <Nav.Link
                 as={Link}
-                to="https://www.Linkedin.com/in/itai-amrofel-8b006a11/"
-                className="test2"
-                style={{ margin: " 0 0.5rem" }}
+                to='https://www.Linkedin.com/in/itai-amrofel-8b006a11/'
+                className='test2'
+                style={{ margin: ' 0 0.5rem' }}
               >
-                <MDBIcon fab icon="linkedin" />
+                <MDBIcon fab icon='linkedin' />
               </Nav.Link>
 
               <Nav.Link
                 as={Link}
-                to="mailto:office@ai-pro.co.il"
-                className="test2"
+                to='mailto:office@ai-pro.co.il'
+                className='test2'
               >
-                <MDBIcon icon="envelope" />
+                <MDBIcon icon='envelope' />
               </Nav.Link>
             </div>
           </Nav>
 
           <div
-            className="col-3 col-md-1 d-flex align-items-center justify-content-center justify-content-md-evenly"
+            className='col-3 col-md-1 d-flex align-items-center justify-content-center justify-content-md-evenly'
             style={{
-              marginRight: "20px",
-              marginLeft: "20px",
+              marginRight: '20px',
+              marginLeft: '20px',
             }}
           >
             <img
               src={usa_color}
-              style={{ width: "30px", marginRight: "5px" }}
-              alt="usa flag"
-              onClick={() => changeLanguage("en")}
+              style={{ width: '30px', marginRight: '5px' }}
+              alt='usa flag'
+              onClick={() => changeLanguage('en')}
             />
             <img
               src={israel_color}
-              style={{ width: "30px" }}
-              alt="usa flag"
-              onClick={() => changeLanguage("he")}
+              style={{ width: '30px' }}
+              alt='usa flag'
+              onClick={() => changeLanguage('he')}
             />
           </div>
         </Navbar.Collapse>
       </Navbar>
     </div>
-  );
+  )
 }
